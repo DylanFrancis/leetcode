56600624b77b09f83a47fa31b9b5cba3b511a91f
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2022-10-22 12:03:54 +0100

    init

---
+++ src/main/java/com/tests/LongestSubstring.java
@@ -0,0 +1,14 @@
+package com.tests;
+
+public class LongestSubstring {
+
+    private int lengthOfLongestSubstring(String theString) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        LongestSubstring longestSubstring = new LongestSubstring();
+
+        System.out.println(longestSubstring.lengthOfLongestSubstring("abcabcbb"));
+    }
+}
