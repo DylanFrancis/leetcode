e34da3ef07a2be2a0d2a56e43dbe45b99800aa68
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2022-11-04 21:57:34 +0000

    ez pz

--- src/main/java/com/tests/besttimetobuyandsellstock/Solution.java
+++ src/main/java/com/tests/besttimetobuyandsellstock/Solution.java
@@ -1,11 +1,38 @@
 package com.tests.besttimetobuyandsellstock;
 
 public class Solution {
-    public int maxProfit(int[] profit) {
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) {
+            return 0;
+        }
 
+        int maxProfit = 0;
 
+        for (int x = 0; x < prices.length - 1; x++) {
 
+            int buy = prices[x];
 
-        return -1;
+            for (int y = x + 1; y < prices.length; y++) {
+
+                int sell = prices[y];
+
+                if (sell <= buy) {
+
+//                    System.out.println("x=" + x + " skip to " + (y - 1));
+                    x = y - 1;
+                    break;
+
+                } else {
+
+                    int profit = sell - buy;
+
+                    if (profit > maxProfit) {
+                        maxProfit = profit;
+                    }
+
+                }
+            }
+        }
+        return maxProfit;
     }
 }
