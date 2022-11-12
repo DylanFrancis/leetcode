675f647ce99b8eed4aa4c77e4e68f7c575e2ef07
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2022-11-12 12:16:22 +0000

    dp ez pz

--- src/main/java/com/tests/climbingstairs/Solution.java
+++ src/main/java/com/tests/climbingstairs/Solution.java
@@ -1,4 +1,51 @@
 package com.tests.climbingstairs;
 
+import java.util.Map;
+
 public class Solution {
+
+    public int climbStairs(int n) {
+
+//        return climb(n, new HashMap<>());
+        return climb2(n, new int[n + 1]);
+
+    }
+
+    private int climb(int n, Map<Integer, Integer> map) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (map.containsKey(n)) {
+
+            return map.get(n);
+
+        } else {
+
+            int a = climb(n - 2, map) + climb(n - 1, map);
+            map.put(n, a);
+
+            return a;
+        }
+    }
+
+    private int climb2(int n, int[] arr) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (arr[n] != 0) {
+
+            return arr[n];
+
+        } else {
+
+            int a = climb2(n - 2, arr) + climb2(n - 1, arr);
+            arr[n] = a;
+
+            return a;
+        }
+    }
 }
--- src/main/java/com/tests/climbingstairs/SolutionTests.java
+++ src/main/java/com/tests/climbingstairs/SolutionTests.java
@@ -1,4 +1,26 @@
 package com.tests.climbingstairs;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SolutionTests {
+
+    @Test
+    public void test1() {
+        doTest(2, 2);
+    }
+
+    @Test
+    public void test2() {
+        doTest(3, 3);
+    }
+
+    @Test
+    public void test3() {
+        doTest(6, 13);
+    }
+
+    private void doTest(int steps, int expected) {
+        Assertions.assertEquals(expected, new Solution().climbStairs(steps), "steps=" + steps);
+    }
 }
