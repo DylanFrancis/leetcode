595b90d2dd50e20269845cd0d8b3c3d1ddfbf7f6
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2022-11-12 12:21:12 +0000

    more

--- src/main/java/com/tests/allpossiblefullbinarytrees/SolutionTests.java
+++ src/main/java/com/tests/allpossiblefullbinarytrees/SolutionTests.java
@@ -1,4 +1,13 @@
 package com.tests.allpossiblefullbinarytrees;
 
+import java.util.Collections;
+import java.util.List;
+
 public class SolutionTests {
+    public List<TreeNode> allPossibleFBT(int n) {
+
+
+
+        return Collections.emptyList();
+    }
 }
