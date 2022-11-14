530560497db8192c8ff5aa159312282c5b37fd55
Dylan <30143500+DylanFrancis@users.noreply.github.com>
2022-11-14 18:15:33 +0000

    this was a hard one

--- src/main/java/com/tests/allpossiblefullbinarytrees/Solution.java
+++ src/main/java/com/tests/allpossiblefullbinarytrees/Solution.java
@@ -1,4 +1,80 @@
 package com.tests.allpossiblefullbinarytrees;
 
+import java.util.*;
+
 public class Solution {
+
+    public List<TreeNode> allPossibleFBT(int n) {
+
+        return buildTree(n, new HashMap<>());
+    }
+
+    private List<TreeNode> buildTree(int n, Map<Integer, List<TreeNode>> map) {
+
+        if (map.containsKey(n)) {
+
+            return map.get(n);
+
+        } else {
+
+            if (n == 1) {
+                return Collections.singletonList(new TreeNode(0));
+            }
+
+            if (n % 2 == 0) {
+
+                map.put(n, Collections.emptyList());
+                return Collections.emptyList();
+
+            } else {
+
+                ArrayList<TreeNode> list = new ArrayList<>();
+
+                for (int x = 1; x < n; x++) {
+
+                    List<TreeNode> leftSubtrees = buildTree(x, map);
+                    List<TreeNode> rightSubtrees = buildTree(n - 1 - x, map);
+
+                    for (TreeNode leftSubtree : leftSubtrees) {
+                        for (TreeNode rightSubtree : rightSubtrees) {
+
+                            TreeNode node = new TreeNode(0);
+                            node.left = leftSubtree;
+                            node.right = rightSubtree;
+                            list.add(node);
+
+                        }
+                    }
+                }
+
+                map.put(n, list);
+
+                return list;
+            }
+        }
+    }
+
+    private List<TreeNode> bruteForce(int n, List<TreeNode> list) {
+
+        if (n == 1) {
+            return Collections.singletonList(new TreeNode(0));
+        }
+
+        if (n % 2 != 0) {
+
+            TreeNode node = new TreeNode(0);
+
+            for (int x = 0; x < n; x++) {
+
+
+
+            }
+
+        } else {
+
+            return Collections.emptyList();
+
+        }
+        return Collections.emptyList();
+    }
 }
--- src/main/java/com/tests/allpossiblefullbinarytrees/SolutionTests.java
+++ src/main/java/com/tests/allpossiblefullbinarytrees/SolutionTests.java
@@ -1,13 +1,33 @@
 package com.tests.allpossiblefullbinarytrees;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.Collections;
 import java.util.List;
 
 public class SolutionTests {
-    public List<TreeNode> allPossibleFBT(int n) {
 
+    @Test
+    public void test() {
 
+        List<TreeNode> answer = new Solution().allPossibleFBT(3);
 
-        return Collections.emptyList();
+        System.out.println(answer);
+
+        answer.forEach(this::print);
+
+//        Assertions.assertEquals();
+
     }
+
+    private void print(TreeNode node) {
+        if (node == null) return;
+
+        System.out.print(node.val + ",");
+
+        print(node.left);
+        print(node.right);
+    }
+
 }
